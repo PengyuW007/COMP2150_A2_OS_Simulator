d65d214e301b69ab8e297a71f0950c4c7416435e
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2021-11-21 00:21:05 -0600

    Create COMP2150 A2

---
+++ cmake-build-release/CMakeFiles/A2B.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "A2B.exe"
+  "A2B.exe.manifest"
+  "A2B.pdb"
+  "CMakeFiles/A2B.dir/main.cpp.obj"
+  "libA2B.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/A2B.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "COMP2150 A2" << std::endl;
+
+    return 0;
+}
