86709d5a949d94ebfdb426f28b79c6b7569226f3
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2021-11-26 22:17:24 -0600

    Initialize OS

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
+#include "Simulation.h"
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "COMP2150 A2" << std::endl;
-
+int main(int argc, char *argv[]){
+    if(argc < 2){
+        cout << "Usage: " << argv[0] << " <file name>" << endl;
+    }
+    cout << "Simulation begins...\n";
+    //Simulation* sim = new Simulation();
+    //sim->runSimulation(argv[1]);
+    cout << "\n...All Processes complete.  Final Summary:\n\n";
+    //sim->summary();
+    cout << "\nEnd of processing.\n";
     return 0;
-}
+}// main
